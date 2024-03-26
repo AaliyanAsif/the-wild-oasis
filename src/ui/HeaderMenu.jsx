41331c69ce250e09df77cb4ee21259e5820b3c04
Aaliyan Asif <93864353+AaliyanAsif@users.noreply.github.com>
2024-03-26 16:33:09 +0500

    app header

---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,28 @@
+import { AiOutlineUser } from "react-icons/ai";
+import React from "react";
+import styled from "styled-components";
+
+import ButtonIcon from "./ButtonIcon";
+import { useNavigate } from "react-router";
+import Logout from "../features/authentication/Logout";
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+`;
+
+export default function HeaderMenu() {
+  const navigate = useNavigate();
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={() => navigate("/account")}>
+          <AiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <Logout />
+      </li>
+    </StyledHeaderMenu>
+  );
+}
